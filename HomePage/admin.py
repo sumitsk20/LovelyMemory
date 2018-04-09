55d2b10454fe862c5d9bd38cb73d27e914d2b99e
waleed <waleed6886@gmail.com>
2018-04-09 17:31:28 +0300

    edit .gitignore

--- HomePage/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Product,CustomerOrderModel
-
-# Register your models here.
-admin.site.register(Product)
-admin.site.register(CustomerOrderModel)
